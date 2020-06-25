6c9945ef31620072094d5cad244ce22cdeb2fb30
dzakirafabillah <61397937+dzakirafabillah@users.noreply.github.com>
2020-06-25 16:00:42 +0700

    recommendation.php
    tambah form

--- recommendation.php
+++ recommendation.php
@@ -1,3 +1,4 @@
+
 <section id="home" class="video-hero" style="height: 500px; background-image: url(images/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
     <div class="overlay"></div>
         <div class="display-t display-t2 text-center">
@@ -13,19 +14,105 @@
             </div>
         </div>
     </section>
+<br><br>
 
-    <div class="colorlib-blog">
+
         <div class="container">
-            <div class="row">
-                <div class="col-md-4 animate-box">
-                    <article>
-                        <h2><a href="#">Building the Mention Sales Application on Unapp</a></h2>
-                        <p class="admin"><span>May 12, 2018</span></p>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-                        <p class="author-wrap"><a href="#" class="author-img" style="background-image: url(images/person1.jpg);"></a> <a href="#" class="author">by Dave Miller</a></p>
-                    </article>
+     
+                <div class="col-md-5 text-center animate-box">
+                    <div class="staff-entry">
+                        <h2 class="form-title">Recommend Me</h2>
+                        <div class="signin-image">
+                            <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                        </div>
+
+                        <form>
+                            <div class="form-row align-items-center">
+                                <div class="col-auto my-1">
+                                <label class="mr-sm-2" for="inlineFormCustomSelect">Genre</label> <br>
+                                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                        <option selected>Choose...</option>
+                                        <option value="1">Berita dan Politik</option>
+                                        <option value="2">Edukasi</option>
+                                        <option value="3">Film dan Animasi</option>
+                                        <option value="4">Gaming</option>
+                                        <option value="5">Hiburan</option>
+                                        <option value="6">Lifestyle</option>
+                                        <option value="7">Musik</option>
+                                </select>
+                                </div>
+                                <div class="col-auto my-1">
+                                
+                                </div>
+                                <br><br>
+                                <div class="col-auto my-1">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <!--
+                            <form method="POST" class="register-form" id="login-form">
+                                <div class="form-group">
+                                    <select class="custom-select custom-select-lg mb-5" required>
+                                        <option value="">Pilih Genre</option>
+                                        <option value="1">Berita dan Politik</option>
+                                        <option value="2">Edukasi</option>
+                                        <option value="3">Film dan Animasi</option>
+                                        <option value="1">Gaming</option>
+                                        <option value="2">Hiburan</option>
+                                        <option value="3">Lifestyle</option>
+                                        <option value="3">Musik</option>
+                                    </select>
+                                </div>
+                                <div class="tombol" >
+                                    <button2 type="submit" name="signin" id="signin" class="btn btn-primary" value="Submit" style="background: #6dabe4; color: #fff; width: auto;
+                                                                                                                          padding: 15px 39px; border-radius: 5px; -moz-border-radius: 5px;
+                                                                                                                        -webkit-border-radius: 5px; -o-border-radius: 5px; -ms-border-radius: 5px;
+                                                                                                                        margin-top: 25px; cursor: pointer; :hover background: #4292dc; "   
+                                    >Submit</button2>
+                                </div>
+                            </form>-->
+                        
+                    </div>
                 </div>
-                <div class="col-md-4 animate-box">
+                
+            
+        </div>
+            <br>
+
+
+        
+
+
+
+
+<!--<br><br>
+  <section>
+   <div class="colorlib-blog">
+        <div class="container">
+           
+            <div class="col-md-4 animate-box">
+                    
+                 <article>
+                         <p>Recommend Me</p>
+                            <div class="form-group">
+                                <select class="custom-select" required>
+                                    <option value="">Pilih Genre</option>
+                                    <option value="1">Berita dan Politik</option>
+                                    <option value="2">Edukasi</option>
+                                    <option value="3">Film dan Animasi</option>
+                                    <option value="1">Gaming</option>
+                                    <option value="2">Hiburan</option>
+                                    <option value="3">Lifestyle</option>
+                                    <option value="3">Musik</option>
+                                </select>
+                                </div>
+                            <div class="invalid-feedback">Example invalid custom select feedback</div>
+                    </article>
+             </div>
+             </section>
+<!--                <div class="col-md-4 animate-box">
                     <article>
                         <h2><a href="#">Building the Mention Sales Application on Unapp</a></h2>
                         <p class="admin"><span>May 12, 2018</span></p>
@@ -108,6 +195,8 @@
                         <li><a href="#">&raquo;</a></li>
                     </ul>
                 </div>
-            </div>
+           
         </div>
-    </div>
+    </div>-->
+
+  
